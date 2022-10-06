39af8fb1d93ccce1eaacc2b8396313d33039acb8
mukeshsin <mukesh@innotechteam.in>
2022-10-06 15:24:59 +0530

    first

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "123456",
+    PASSWORD: "singh@2023",
     DB: "testdb",
     dialect: "mysql",
     pool: {
--- server.js
+++ server.js
@@ -5,20 +5,20 @@ const cors= require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8080"
+  origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- const db = require("./nodeapp/models");
+ const db = require("./app/models");
 
  db.sequelize.sync();
 app.get("/", (req, res) => {
   res.json({ message: "this is an example" });
 });
- require("./nodeapp/routes/tutorial.routes")(app);
+ require("./app/routes/tutorial.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
